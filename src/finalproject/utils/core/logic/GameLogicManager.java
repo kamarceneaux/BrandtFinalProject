be11c7200923236ff22edf2e63b569c372013566
Kameron Arceneaux <kameronarceneaux05@gmail.com>
2024-03-30 11:39:08 -0500

    Intialized File

---
+++ src/finalproject/utils/core/logic/GameLogicManager.java
@@ -0,0 +1,12 @@
+package finalproject.utils.core.logic;
+
+import finalproject.utils.core.Cart;
+
+public class GameLogicManager {
+    private int totalItems;
+    private Cart correctItemsCart;
+
+    public void startLogic(){
+
+    }
+}
